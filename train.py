990498afb29d3ceff296e5c0fb353725074126ec
Do Yoon Kim <ddoyoon.kim@gmail.com>
2021-05-24 09:40:34 +0000

    Fix wandb init and add run name option

--- train.py
+++ train.py
@@ -5,9 +5,13 @@ from dkt import trainer
 import torch
 from dkt.utils import setSeeds
 import wandb
+from datetime import datetime
+from pytz import timezone
+
+
 def main(args):
     wandb.login()
-    
+
     setSeeds(42)
     device = "cuda" if torch.cuda.is_available() else "cpu"
     args.device = device
@@ -15,12 +19,15 @@ def main(args):
     preprocess = Preprocess(args)
     preprocess.load_train_data(args.file_name)
     train_data = preprocess.get_train_data()
-    
+
     train_data, valid_data = preprocess.split_data(train_data)
-    
-    wandb.init(project='dkt', config=vars(args))
+
+    if not args.run_name:
+        args.run_name = datetime.now(timezone("Asia/Seoul")).strftime("%Y-%m-%d-%H:%M:%S")
+
+    wandb.init(project='p-stage-4', entity='lastffang', name='-'.join([args.prefix, args.run_name]), config=vars(args))
     trainer.run(args, train_data, valid_data)
-    
+
 
 if __name__ == "__main__":
     args = parse_args(mode='train')
