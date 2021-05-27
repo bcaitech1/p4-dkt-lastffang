0814a20bde6a334c869d50ad479a8115ce4e14ac
newspring97 <newbom26@gmail.com>
2021-05-27 09:54:06 +0000

    add run_with_json.py & default.json

---
+++ run_with_json.py
@@ -0,0 +1,25 @@
+import json
+import argparse
+from subprocess import call
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--config", dest="cfg", type=str, default='configs/default.json')
+
+args = parser.parse_args()
+
+with open(args.cfg) as f:
+    cfg = json.load(f)
+
+
+arguments = []
+
+for k in cfg:
+    arguments.append("--" + k)
+    arguments.append(cfg[k])
+
+# Train Model
+call(["python", "train.py"] + arguments)
+
+# Inference file
+call(["python", "inference.py"] + arguments)
