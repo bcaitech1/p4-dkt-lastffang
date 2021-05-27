7f01d254726c08399390dee45779b37986757188
GY-Jeong <jyjg1124@gmail.com>
2021-05-27 15:03:17 +0000

    change n_epochs to default : 20

--- args.py
+++ args.py
@@ -34,7 +34,7 @@ def parse_args(mode):
     parser.add_argument('--drop_out', default=0.2, type=float, help='drop out rate')
 
     # 훈련
-    parser.add_argument('--n_epochs', default=100, type=int, help='number of epochs')
+    parser.add_argument('--n_epochs', default=20, type=int, help='number of epochs')
     parser.add_argument('--batch_size', default=64, type=int, help='batch size')
     parser.add_argument('--lr', default=0.0001, type=float, help='learning rate')
     parser.add_argument('--clip_grad', default=10, type=int, help='clip grad')
