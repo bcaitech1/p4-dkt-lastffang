48db45024d88cf7e8da4b0392169f25cbdd3caa0
GY-Jeong <jyjg1124@gmail.com>
2021-05-31 16:31:02 +0000

    solve conflict(loss args)

--- dkt/trainer.py
+++ dkt/trainer.py
@@ -80,7 +80,7 @@ def train(train_loader, model, optimizer, args):
         preds = model(input)
         targets = input[0] # correct
         # print(preds.dtype, targets.dtype)
-        loss = compute_loss(preds, targets)
+        loss = compute_loss(preds, targets, args)
         update_params(loss, model, optimizer, args)
 
         if step % args.log_steps == 0:
@@ -281,17 +281,15 @@ def process_batch(batch, args):
 
     return tuple(features)
 
-
 # loss계산하고 parameter update!
-def compute_loss(preds, targets):
+def compute_loss(preds, targets, args):
     """
     Args :
         preds   : (batch_size, max_seq_len)
         targets : (batch_size, max_seq_len)
-
     """
-    loss = get_criterion(preds, targets)
-    #마지막 시퀀드에 대한 값만 loss 계산
+    loss = get_criterion(preds, targets, args)
+    #마지막 시퀀스에 대한 값만 loss 계산
     loss = loss[:,-1]
     loss = torch.mean(loss)
     return loss
