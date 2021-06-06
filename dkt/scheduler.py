8565f0cb1ab963e91e432e028d0bdd34afec3605
scarlettJin <30853777+scarlettJin@users.noreply.github.com>
2021-06-06 22:46:47 +0900

    StepLR,ExponentialLR,CosineAnnealingLR,CosineAnnealingWarmRestarts추가

--- dkt/scheduler.py
+++ dkt/scheduler.py
@@ -1,6 +1,4 @@
-
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-
+from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau, CosineAnnealingLR,ExponentialLR,CosineAnnealingWarmRestarts
 from transformers import get_linear_schedule_with_warmup
 
 
@@ -8,7 +6,14 @@ def get_scheduler(optimizer, args):
     if args.scheduler == 'plateau':
         scheduler = ReduceLROnPlateau(optimizer, patience=args.plateau_patience, factor=args.plateau_factor, mode='max', verbose=True)
     elif args.scheduler == 'linear_warmup':
-        scheduler = get_linear_schedule_with_warmup(optimizer,
-                                                    num_warmup_steps=args.warmup_steps,
-                                                    num_training_steps=args.total_steps)
+        scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps= args.warmup_steps,num_training_steps=args.total_steps)
+    elif args.scheduler == 'step_lr':
+        scheduler = StepLR(optimizer, step_size=args.step_size, gamma=args.gamma)
+    elif args.scheduler == 'exp_lr':
+        scheduler = ExponentialLR(optimizer, gamma=args.gamma)
+    elif args.scheduler == 'cosine_annealing':
+        scheduler = CosineAnnealingLR(optimizer, T_max=args.t_max, eta_min=args.eta_min)
+    elif args.scheduler == 'cosine_annealing_warmstart':
+        scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=args.T_0, T_mult=1, eta_min=args.eta_min, last_epoch=-1)
+
     return scheduler
