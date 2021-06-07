e6e1853bc04262cf3eaa3fb233dbb5908984e37f
alinghi <alinghi@kaist.ac.kr>
2021-06-07 20:05:51 +0900

    [Modify] Add 3 loss func
    Add BCEWithLogitsLoss, MSELoss, L1Loss.

--- dkt/criterion.py
+++ dkt/criterion.py
@@ -1,8 +1,16 @@
-
 import torch.nn as nn
 
 
 def get_criterion(pred, target, args):
     if args.criterion == 'BCE':
+        loss = nn.BCELoss(reduction="none")
+    elif args.criterion == "BCELogit":
+        loss = nn.BCEWithLogitsLoss(reduction="none")
+    elif args.criterion == "MSE":
+        loss = nn.MSELoss(reduction="none")
+    elif args.criterion == "L1":
+        loss = nn.L1Loss(reduction="none")
+    # NLL, CrossEntropy not available
+    else: # set default
         loss = nn.BCELoss(reduction="none")
     return loss(pred, target)
